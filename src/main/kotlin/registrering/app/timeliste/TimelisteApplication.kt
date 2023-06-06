9161b1ac5fde22a4979f08a938d60c48d1b423a2
tnbossi123 <blerton-rustemi@hotmail.no>
2023-06-06 22:02:41 +0200

    Initial COmmit

---
+++ src/main/kotlin/registrering/app/timeliste/TimelisteApplication.kt
@@ -0,0 +1,11 @@
+package registrering.app.timeliste
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimelisteApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimelisteApplication>(*args)
+}
---
+++ src/test/kotlin/registrering/app/timeliste/TimelisteApplicationTests.kt
@@ -0,0 +1,13 @@
+package registrering.app.timeliste
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimelisteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
